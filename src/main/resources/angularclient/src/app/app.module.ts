5e45e9c91144ce3516511866cd5f3fd3d4a01e9e
Markus Ackermann <markus.ackermann@europace.de>
2020-07-17 19:08:48 +0200

    Add user-form component and app-routing

--- src/main/resources/angularclient/src/app/app.module.ts
+++ src/main/resources/angularclient/src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule } from "@angular/router";
 import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { FormsModule } from "@angular/forms";
+import {UserService} from "./service/user.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    UserFormComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterModule
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
