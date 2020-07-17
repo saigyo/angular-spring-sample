b9448d6c3aedfa812782bf4fb37fd3f7bef58f9f
Markus Ackermann <markus.ackermann@europace.de>
2020-07-17 19:20:17 +0200

    Reformat code

--- src/main/resources/angularclient/src/app/app.module.ts
+++ src/main/resources/angularclient/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from "./app-routing.module";
-import { RouterModule } from "@angular/router";
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { FormsModule } from "@angular/forms";
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterModule} from "@angular/router";
+import {UserListComponent} from './user-list/user-list.component';
+import {UserFormComponent} from './user-form/user-form.component';
+import {FormsModule} from "@angular/forms";
 import {UserService} from "./service/user.service";
 import {HttpClientModule} from "@angular/common/http";
 
@@ -26,4 +26,5 @@ import {HttpClientModule} from "@angular/common/http";
   providers: [UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
