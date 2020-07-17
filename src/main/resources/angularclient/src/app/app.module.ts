f8bb1c32162cda10e40a1b3efd5e67d62b781d4d
Markus Ackermann <markus.ackermann@europace.de>
2020-07-17 18:25:07 +0200

    Add router module

--- src/main/resources/angularclient/src/app/app.module.ts
+++ src/main/resources/angularclient/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
